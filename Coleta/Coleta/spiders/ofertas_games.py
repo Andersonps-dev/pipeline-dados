425f5b32a76355c114824e54cbb58cdad6e05d0c
Andersonps-dev <aaandersonpereira45265@gmail.com>
2024-11-15 15:51:30 -0300

    Problema com o Titulo rescolvido, mapeando os produtos.

--- Coleta/Coleta/spiders/ofertas_games.py
+++ Coleta/Coleta/spiders/ofertas_games.py
@@ -10,13 +10,13 @@ class OfertasGamesSpider(scrapy.Spider):
 
         for produto in produtos:
             yield {
-                'highlight': produto.css('span.andes-money-amount__fraction::text').get(),
-                'titulo': produto.css('span.andes-money-amount__fraction::text').get(),
-                'brand': produto.css('span.andes-money-amount__fraction::text').get(),
-                'nota': produto.css('span.andes-money-amount__fraction::text').get(),
-                'preco_anterior': produto.css('span.andes-money-amount__fraction::text').get(),
-                'preco_atual': produto.css('span.andes-money-amount__fraction::text').get(),
-                'preco_fracao': produto.css('span.andes-money-amount__fraction::text').get(),
-                'porcentagem_desconto': produto.css('span.andes-money-amount__fraction::text').get(),
-                'detalhe_envio': produto.css('span.andes-money-amount__fraction::text').get()
+                # 'highlight': produto.css('span.andes-money-amount__fraction::text').get(),
+                'titulo': produto.css('h2.poly-box.poly-component__title a::text').get(),
+                # 'brand': produto.css('span.andes-money-amount__fraction::text').get(),
+                # 'nota': produto.css('span.andes-money-amount__fraction::text').get(),
+                # 'preco_anterior': produto.css('span.andes-money-amount__fraction::text').get(),
+                'preco_atual': produto.css('span.andes-money-amount__fraction::text').get()
+                # 'preco_fracao': produto.css('span.andes-money-amount__fraction::text').get(),
+                # 'porcentagem_desconto': produto.css('span.andes-money-amount__fraction::text').get(),
+                # 'detalhe_envio': produto.css('span.andes-money-amount__fraction::text').get()
                    }
