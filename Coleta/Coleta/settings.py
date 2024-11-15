3b26810b24629841581ad47a1917fba0026bebec
Andersonps-dev <aaandersonpereira45265@gmail.com>
2024-11-15 13:20:33 -0300

    Agente user configurado e site alterado

--- Coleta/Coleta/settings.py
+++ Coleta/Coleta/settings.py
@@ -14,7 +14,6 @@ NEWSPIDER_MODULE = "Coleta.spiders"
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "Coleta (+http://www.yourdomain.com)"
 
--- Coleta/Coleta/spiders/ofertas.py
+++ Coleta/Coleta/spiders/ofertas.py
@@ -4,7 +4,7 @@ import scrapy
 class OfertasSpider(scrapy.Spider):
     name = "ofertas"
     allowed_domains = ["www.mercadolivre.com.br"]
-    start_urls = ["https://www.mercadolivre.com.br/ofertas?category=MLB1574#filter_applied=category&filter_position=3&origin=qcat"]
+    start_urls = ["https://www.mercadolivre.com.br/ofertas?category=MLB1648#filter_applied=category&filter_position=3&origin=qcat"]
 
     def parse(self, response):
         pass
