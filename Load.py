8fa838260366c296bdba7e36dc3c42e57ae44311
Andersonps-dev <aaandersonpereira45265@gmail.com>
2024-11-15 11:45:05 -0300

    Estrutura do Projeto Criada

---
+++ Load.py
@@ -0,0 +1,14 @@
+import requests
+import scrapy
+import time
+import pandas as pd
+import asyncio
+from telegram import Bot
+import os
+from dotenv import load_dotenv
+import sqlite3
+import time
+
+class LoadData:
+    def __init__(self):
+        pass
