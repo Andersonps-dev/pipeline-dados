389ff4148f952054827fec3b527524ed7af4a69e
Andersonps-dev <aaandersonpereira45265@gmail.com>
2024-11-17 18:57:26 -0300

    Tratamento de dados feito, dados salvos no sqlite3, banco dados dados_coletados

--- Src/Transformacao/main.py
+++ Src/Transformacao/main.py
@@ -7,9 +7,17 @@ import pandas as pd
 import asyncio
 from telegram import Bot
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 import psycopg2
 from sqlalchemy import create_engine
+import sqlite3
+import asyncio
+from telegram import Bot
+from tabulate import tabulate
+
+pd.set_option("display.max_columns", None)  # Mostra todas as colunas
+pd.set_option("display.max_rows", None)     # Mostra todas as linhas
 
 class Transformacao:
     def __init__(self):
@@ -21,8 +29,60 @@ class Transformacao:
         self.POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
         self.pasta_dados = r'..\pipeline-dados\Dados'
+    
+    def tratar_base(self, nome_arquivo=None, nome_tabela_bd=None, nome_bd='dados_coletados.bd'):
+        caminho = os.path.join(self.pasta_dados, nome_arquivo)
+        
+        df = pd.read_json(caminho, lines=True, dtype={"preco_anterior": str, "fracao_preco_anterior":str, "preco_atual":str, "fracao_preco_atual":str})
+        def limpar_numero(valor):
+            if pd.isnull(valor):  # Se o valor for nulo, mantenha como está
+                return valor
+            return int(str(valor).replace('.', '').replace(',', ''))
 
-    def criar_conexao(self):
+        colunas_substituir_none = ['preco_anterior', 'fracao_preco_anterior', 'preco_atual',
+       'fracao_preco_atual']
+        df[colunas_substituir_none] = df[colunas_substituir_none].replace(['None', None], 0)
+
+        df["preco_anterior"] = df["preco_anterior"].apply(limpar_numero)
+        df["fracao_preco_anterior"] = df["fracao_preco_anterior"].apply(limpar_numero)
+        df["preco_atual"] = df["preco_atual"].apply(limpar_numero)
+        df["fracao_preco_atual"] = df["fracao_preco_atual"].apply(limpar_numero)
+        
+        df['preco_anterior'] = df["preco_anterior"].astype(str) + "," + df["fracao_preco_anterior"].astype(str)
+        df['preco_atual'] = df["preco_atual"].astype(str) + "," + df["fracao_preco_atual"].astype(str)
+        
+        df = df.drop(columns=['fracao_preco_anterior', 'fracao_preco_atual'])
+        
+        df['data_coleta']  = datetime.now()
+
+        df.to_sql(nome_tabela_bd, self.criar_conexao_sqlite3(nome_bd), if_exists='append', index=False)
+
+
+    def criar_conexao_sqlite3(self, db_name):
+        conn = sqlite3.connect(db_name)
+        return conn
+
+    def setup_database(self, conn, nome_tabela):
+        cursor = conn.cursor()
+        cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {nome_tabela} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                highlight TEXT,
+                titulo TEXT,
+                vendido_por TEXT,
+                nota TEXT,
+                total_avaliacoes INTEGER,
+                preco_anterior FLOAT,
+                preco_atual FLOAT,
+                porcentagem_desconto TEXT,
+                detalhe_envio TEXT,
+                detalhe_envio_2 TEXT,
+                data_coleta DATETIME
+            )
+        ''')
+        conn.commit()
+
+    def criar_conexao_postgres(self):
         try:
             conn = psycopg2.connect(
                 dbname=self.POSTGRES_DB,
@@ -36,8 +96,8 @@ class Transformacao:
             print(f"Erro ao conectar ao banco de dados: {e}")
             return None
     
-    def testar_conexao(self):
-        conn = self.criar_conexao()
+    def testar_conexao_postgres(self):
+        conn = self.criar_conexao_postgres()
         if conn:
             try:
                 cursor = conn.cursor()
@@ -50,3 +110,11 @@ class Transformacao:
                 conn.close()
         else:
             print("Conexão falhou.")
+        
+    def executor(self):
+        conn = self.criar_conexao_sqlite3("dados.db")
+        self.setup_database(conn, "dados_games")
+        self.tratar_base(nome_arquivo="dados_games.jsonl", nome_tabela_bd="dados_games")
+        
+exe = Transformacao()
+exe.executor()
