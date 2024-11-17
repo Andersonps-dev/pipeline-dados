0a59b94d44c6716365b6a9a92d9238f21dcea153
Andersonps-dev <aaandersonpereira45265@gmail.com>
2024-11-17 15:08:52 -0300

    Conexão Banco Postgres

--- Src/Transformacao/main.py
+++ Src/Transformacao/main.py
@@ -1,4 +1,30 @@
 import pandas as pd
 import sqlite3
 from datetime import datetime
+import requests
+import time
+import pandas as pd
+import asyncio
+from telegram import Bot
+import os
+from dotenv import load_dotenv
+import psycopg2
+from sqlalchemy import create_engine
 
+class Transformacao:
+    def __init__(self):
+        self.POSTGRES_DB = os.getenv("POSTGRES_DB")
+        self.POSTGRES_USER = os.getenv("POSTGRES_USER")
+        self.POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+        self.POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+        self.POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+    def criar_conexao(self):
+        conn = psycopg2.connect(
+            dbname=self.POSTGRES_DB,
+            user=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT
+        )
+        return conn
