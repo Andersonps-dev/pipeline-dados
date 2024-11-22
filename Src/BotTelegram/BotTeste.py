58274d00602683bf757073407d343cccbd813c74
Andersonps-dev <aaandersonpereira45265@gmail.com>
2024-11-21 22:14:12 -0300

    BOT TELEGRAN COM TOPICO.

--- Src/BotTelegram/BotTeste.py
+++ Src/BotTelegram/BotTeste.py
@@ -10,10 +10,11 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TOPIC_ID = os.getenv('TELEGRAM_TOPIC_ID')
 bot = Bot(token=TOKEN)
 
 async def send_telegram_message(text):
-    await bot.send_message(chat_id=CHAT_ID, text=text)
+    await bot.send_message(chat_id=CHAT_ID, text=text, message_thread_id=TOPIC_ID)
 async def main():
     try:
         while True:
