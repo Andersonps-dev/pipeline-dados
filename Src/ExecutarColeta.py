6ef9b13e6c05f936be658f994e1e02ca818322ef
Andersonps-dev <aaandersonpereira45265@gmail.com>
2024-12-05 09:21:35 -0300

    função para salvar os dados antigos antes de executar a nova coleta

--- Src/ExecutarColeta.py
+++ Src/ExecutarColeta.py
@@ -2,17 +2,64 @@ import subprocess
 import os
 import sys
 
+from Transformacao.main import Transformacao
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-class ExecutorDeScripts:
+class ExecutorDeScripts(Transformacao):
     def __init__(self):
+        super().__init__()
+        
         self.diretorio_principal = os.path.abspath(os.getcwd())
         self.python_venv = os.path.join(self.diretorio_principal, ".venv", "Scripts", "python.exe")
 
-    def salvar_dados_antigos(self):
+    def salvar_dados_antigos(self, tabela_atual, tabela_anterior):
+        conn = self.criar_conexao_sqlite3("dados_coletados.db")
+        cursor = conn.cursor()
+
+        cursor.execute(f"""
+            SELECT *,
+                (preco_anterior - preco_atual) AS desconto_reais 
+            FROM {tabela_atual}
+            WHERE porcentagem_desconto >= 40 OR desconto_reais >= 600
+            ORDER BY porcentagem_desconto DESC 
+            LIMIT 100
+        """)
+        resultado = cursor.fetchall()
         
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS {tabela_anterior} (
+                highlight TEXT,
+                titulo TEXT,
+                link TEXT,
+                vendido_por TEXT,
+                nota REAL,
+                total_avaliacoes INTEGER,
+                preco_anterior REAL,
+                preco_atual REAL,
+                porcentagem_desconto REAL,
+                detalhe_envio TEXT,
+                detalhe_envio_2 TEXT,
+                data_coleta TEXT,
+                desconto_reais REAL
+            )
+        """)
+        
+        for row in resultado:
+            cursor.execute(f"""
+                INSERT INTO {tabela_anterior} (
+                    highlight, titulo, link, vendido_por, nota, total_avaliacoes, 
+                    preco_anterior, preco_atual, porcentagem_desconto, detalhe_envio, 
+                    detalhe_envio_2, data_coleta, desconto_reais
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, row)
+
+        conn.commit()
+        cursor.close()
+        conn.close()
     
     def executar_scrapy(self, coleta, nome_arquivo):
+        self.salvar_dados_antigos(nome_arquivo, f"{nome_arquivo}_tabela_anterior")
         try:
             scrapy_dir = os.path.join(self.diretorio_principal, "Src", "Coleta", "Coleta", "spiders")
             os.chdir(scrapy_dir)
