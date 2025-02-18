5e3f2d49c581b84bae692d8fd8edb5f4e94a4888
Andersonps-dev <aaandersonpereira45265@gmail.com>
2025-02-17 22:26:49 -0300

    retorno main

--- Src/Transformacao/main.py
+++ Src/Transformacao/main.py
@@ -27,52 +27,38 @@ class Transformacao(NotifyOfferBot):
     def tratar_base(self, conn=None, nome_arquivo=None, nome_tabela_bd=None, nome_bd='dados_coletados.db', topic_id=None):
         caminho = os.path.join(self.pasta_dados, nome_arquivo)
         
-        df = pd.read_json(caminho, lines=True)
-        
-        # Conversão de tipos
-        df["preco_anterior"] = df["preco_anterior"].astype(str)
-        df["fracao_preco_anterior"] = df["fracao_preco_anterior"].astype(str)
-        df["preco_atual"] = df["preco_atual"].astype(str)
-        df["fracao_preco_atual"] = df["fracao_preco_atual"].astype(str)
-
-        # Função para limpar números
+        df = pd.read_json(caminho, lines=True, dtype={"preco_anterior": str, "fracao_preco_anterior":str, "preco_atual":str, "fracao_preco_atual":str})
         def limpar_numero(valor):
-            if pd.isnull(valor) or valor == '':
-                return 0
+            if pd.isnull(valor):
+                return valor
             return int(str(valor).replace('.', '').replace(',', ''))
 
-        # Substituição de valores None
         colunas_substituir_none = ['preco_anterior', 'fracao_preco_anterior', 'preco_atual', 'fracao_preco_atual']
-        df[colunas_substituir_none] = df[colunas_substituir_none].replace([None, 'None', ''], 0)
+        df[colunas_substituir_none] = df[colunas_substituir_none].replace(['None', None], 0)
 
-        # Aplicação da função de limpeza
         df["preco_anterior"] = df["preco_anterior"].apply(limpar_numero)
         df["fracao_preco_anterior"] = df["fracao_preco_anterior"].apply(limpar_numero)
         df["preco_atual"] = df["preco_atual"].apply(limpar_numero)
         df["fracao_preco_atual"] = df["fracao_preco_atual"].apply(limpar_numero)
-
-        # Concatenação e conversão para float
+        
         df['preco_anterior'] = df["preco_anterior"].astype(str) + "," + df["fracao_preco_anterior"].astype(str)
         df['preco_atual'] = df["preco_atual"].astype(str) + "," + df["fracao_preco_atual"].astype(str)
-        df['preco_anterior'] = df['preco_anterior'].str.replace(',', '.').astype(float)
-        df['preco_atual'] = df['preco_atual'].str.replace(',', '.').astype(float)
-
-        # Remoção de colunas desnecessárias
+        
         df = df.drop(columns=['fracao_preco_anterior', 'fracao_preco_atual'])
 
-        # Extração de porcentagem de desconto
-        df['porcentagem_desconto'] = df['porcentagem_desconto'].str.extract('(\d+)').fillna(0).astype(int)
-
-        # Ordenação e remoção de duplicatas
+        df['porcentagem_desconto'] = df["porcentagem_desconto"].str.extract('(\d+)')
+        df['porcentagem_desconto'] = df['porcentagem_desconto'].fillna(0)
+        df['porcentagem_desconto'] = df['porcentagem_desconto'].astype(int)
+        
         df = df.sort_values('porcentagem_desconto', ascending=True)
         df = df.drop_duplicates(subset=['titulo'], keep='last')
-
-        # Adição de colunas adicionais
-        df['data_coleta'] = datetime.now()
+        df['data_coleta']  = datetime.now()
         df['topico_de_envio'] = f"{topic_id}"
+
+        df['preco_anterior'] = df['preco_anterior'].str.replace(',', '.').astype(float)
+        df['preco_atual'] = df['preco_atual'].str.replace(',', '.').astype(float)
         df['desconto_reais'] = df['preco_anterior'] - df['preco_atual']
 
-        # Função para calcular pontuação
         def calcular_pontuacao(row, lojas_vistas):
             pontuacao = 0
 
@@ -136,11 +122,10 @@ class Transformacao(NotifyOfferBot):
 
             return pontuacao
 
-        # Aplicação da função de pontuação
         lojas_vistas = set()
+
         df["pontuacao"] = df.apply(calcular_pontuacao, axis=1, lojas_vistas=lojas_vistas)
 
-        # Classificação de relevância
         def classificar_relevancia(pontuacao):
             if pontuacao >= 8:
                 return "1"
@@ -155,9 +140,9 @@ class Transformacao(NotifyOfferBot):
 
         df["relevancia"] = df["pontuacao"].apply(classificar_relevancia)
 
-        # Filtragem e salvamento no banco de dados
-        df = df[df['porcentagem_desconto'] != 0]
-        df.to_sql(nome_tabela_bd, criar_conexao_sqlite3(nome_bd), if_exists='replace', index=False)
+        df = df[df['porcentagem_desconto'] !=0]
+
+        df.to_sql(nome_tabela_bd, self.criar_conexao_sqlite3(nome_bd), if_exists='replace', index=False)
 
     def criar_conexao_sqlite3(self, db_name):
         conn = sqlite3.connect(db_name)
